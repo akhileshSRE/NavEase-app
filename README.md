b0bcbab1f266e9996c47d3f6e0b27e8c865ac4aa
Akhilesh Kumar <akhilesh@Akhileshs-MacBook-Pro.local>
2024-05-30 17:30:57 +0530

    initial commit of codebase

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NavEase-app
 NavEase application which acts as admin + api service
+
+docker build -t navease-app .
+d
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
